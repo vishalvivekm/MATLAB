a932ed7876c4fa1f8259dab09163a52e74a99d21
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2023-11-02 00:41:30 +0530

    add more switch-case statements
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

---
+++ conditionals/switch_case_2.m
@@ -0,0 +1,15 @@
+grade = 'B';
+   switch(grade)
+   case 'A' 
+      fprintf('Excellent!\n' );
+   case 'B' 
+      fprintf('Well done\n' );
+   case 'C' 
+      fprintf('Well done\n' );
+   case 'D'
+      fprintf('You passed\n' );
+   case 'F' 
+      fprintf('Better try again\n' );
+   otherwise
+      fprintf('Invalid grade\n' );
+   end
